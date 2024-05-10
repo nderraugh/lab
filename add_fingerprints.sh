5677d75b5dfd4af3309d24c7d73ae25046b8e041
Neil Derraugh <neil.derraugh@yoppworks.com>
2024-05-10 16:35:31 -0400

    Update
    - changing IP addresses
    - cgroups text has changed in recent versions of ubuntu
    - ping ot latest/stable away from 1.20

--- add_fingerprints.sh
+++ add_fingerprints.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-ssh-keyscan -t ecdsa 192.168.12.60 >> ~/.ssh/known_hosts
-ssh-keyscan -t ecdsa 192.168.12.61 >> ~/.ssh/known_hosts
-ssh-keyscan -t ecdsa 192.168.12.62 >> ~/.ssh/known_hosts
-ssh-keyscan -t ecdsa 192.168.12.63 >> ~/.ssh/known_hosts
+ssh-keyscan -t ecdsa 192.168.0.60 >> ~/.ssh/known_hosts
+ssh-keyscan -t ecdsa 192.168.0.61 >> ~/.ssh/known_hosts
+ssh-keyscan -t ecdsa 192.168.0.62 >> ~/.ssh/known_hosts
+ssh-keyscan -t ecdsa 192.168.0.63 >> ~/.ssh/known_hosts
